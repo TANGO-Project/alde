766496bdb10a2b8504a468b3714722fd4e040243
David Garcia Perez <david.garciaperez@atos.net>
2016-11-24 11:48:05 +0100

    adding the unit tests that validate the Object-Relational mapping between the Testbed class and de Node class

--- src/unittest/python/sqlalchemy_mapping_tests/testbed_mapping_tests.py
+++ src/unittest/python/sqlalchemy_mapping_tests/testbed_mapping_tests.py
@@ -7,7 +7,7 @@
 # This code is licensed under an Apache 2.0 license. Please, refer to the LICENSE.TXT file for more information
 
 from sqlalchemy_mapping_tests.mapping_tests import MappingTest
-from model.models import Testbed
+from model.models import Testbed, Node
 
 class TestbedMappingTest(MappingTest):
     """
@@ -43,3 +43,63 @@ class TestbedMappingTest(MappingTest):
         self.session.delete(testbed)
         count = self.session.query(Testbed).filter_by(name='name').count()
         self.assertEquals(0, count)
+
+    def test_testbed_node_relation(self):
+        """
+        Unit test that verifies that the correct relation between
+        the testbed class and the node class is built
+        """
+
+        # We create a testbed
+        testbed = Testbed("name", True, "slurm", "ssh", "user@server", ['slurm'])
+
+        # We create several nodes
+        testbed.nodes = [
+                           Node("node1", True),
+                           Node("node2", False),
+                           Node("node3", True) ]
+
+        # We save everything to the db
+        self.session.add(testbed)
+        self.session.commit()
+
+        # We retrieve and verify that the nodes are there
+        testbed = self.session.query(Testbed).filter_by(name='name').first()
+
+        self.assertEquals(3, len(testbed.nodes))
+        self.assertEquals("node1", testbed.nodes[0].name)
+        self.assertTrue(testbed.nodes[0].information_retrieved)
+        self.assertEquals("node2", testbed.nodes[1].name)
+        self.assertFalse(testbed.nodes[1].information_retrieved)
+        self.assertEquals("node3", testbed.nodes[2].name)
+        self.assertTrue(testbed.nodes[2].information_retrieved)
+
+        # lets remove a node from testbed
+        testbed.remove_node(testbed.nodes[2])
+
+        # We commit the state and it should be updated
+        self.session.commit()
+        testbed = self.session.query(Testbed).filter_by(name='name').first()
+        self.assertEquals(2, len(testbed.nodes))
+        self.assertEquals("node1", testbed.nodes[0].name)
+        self.assertTrue(testbed.nodes[0].information_retrieved)
+        self.assertEquals("node2", testbed.nodes[1].name)
+        self.assertFalse(testbed.nodes[1].information_retrieved)
+
+        # Lets delete the node directly from the session
+        self.session.delete(testbed.nodes[1])
+        self.session.commit()
+        testbed = self.session.query(Testbed).filter_by(name='name').first()
+        self.assertEquals(1, len(testbed.nodes))
+        self.assertEquals("node1", testbed.nodes[0].name)
+        self.assertTrue(testbed.nodes[0].information_retrieved)
+
+        # It should be only one node in the db
+        nodes = self.session.query(Node).all()
+        self.assertEquals(2, len(nodes))
+        self.assertEquals("node1", nodes[0].name)
+        self.assertTrue(nodes[0].information_retrieved)
+        self.assertIsNotNone(nodes[0].testbed)
+        self.assertEquals("node3", nodes[1].name)
+        self.assertTrue(nodes[1].information_retrieved)
+        self.assertIsNone(nodes[1].testbed)
