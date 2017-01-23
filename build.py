42d6fb92b86939bbdca7ba314d05b148af1b5a1d
David Garcia Perez <david.garciaperez@atos.net>
2017-01-23 12:50:17 +0100

    reseting build.py to normal status

--- build.py
+++ build.py
@@ -11,10 +11,10 @@ from pybuilder.core import init, use_plugin
 
 use_plugin("python.core")
 use_plugin("python.unittest")
-#use_plugin("python.coverage")
+use_plugin("python.coverage")
 use_plugin("python.install_dependencies")
 use_plugin("python.distutils")
-#use_plugin("python.sonarqube")
+use_plugin("python.sonarqube")
 
 default_task = ["clean", "publish"]
 
