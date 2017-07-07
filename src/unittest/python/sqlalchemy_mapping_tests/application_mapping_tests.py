67bb06160cd0bce4406b8fc782100d678258193a
David Garcia Perez <davidgp@davidgp.com>
2017-07-07 12:25:07 +0200

    adding test that verifies that the relation between the class ExecutionScript and Application is well done in SQLAlchemy

--- src/unittest/python/sqlalchemy_mapping_tests/application_mapping_tests.py
+++ src/unittest/python/sqlalchemy_mapping_tests/application_mapping_tests.py
@@ -7,7 +7,7 @@
 # This code is licensed under an Apache 2.0 license. Please, refer to the LICENSE.TXT file for more information
 
 from sqlalchemy_mapping_tests.mapping_tests import MappingTest
-from model.models import Application
+from model.models import Application, ExecutionScript
 from model.base import db
 
 class ApplicationMappingTest(MappingTest):
@@ -42,3 +42,47 @@ class ApplicationMappingTest(MappingTest):
         db.session.delete(application_2)
         count = db.session.query(Application).filter_by(name='pepito').count()
         self.assertEquals(0, count)
+
+    def test_application_execution_script_relation(self):
+        """
+        Unit tests that verifies the correct relation between the Application
+        class and the ExecutionScript class is built
+        """
+
+        # We create an application
+        application = Application("AppName")
+
+
+        # We create several ExecutionScripts
+        application.execution_scripts = [
+            ExecutionScript("ls", "slurm:sbatch", "-x1"),
+            ExecutionScript("cd", "slurm:xxx", "-x2"),
+            ExecutionScript("rm", "asdf", "-rf /")]
+
+        # We save everything to the db
+        db.session.add(application)
+        db.session.commit()
+
+        # We retrieve the application and verify taht the execution_scripts are there
+        application = db.session.query(Application).filter_by(name='AppName').first()
+
+        self.assertEquals(3, len(application.execution_scripts))
+        self.assertEquals("ls", application.execution_scripts[0].command)
+        self.assertEquals("cd", application.execution_scripts[1].command)
+        self.assertEquals("rm", application.execution_scripts[2].command)
+
+        # lets delete a execution_script directly
+        db.session.delete(application.execution_scripts[0])
+        db.session.commit()
+
+        application = db.session.query(Application).filter_by(name='AppName').first()
+        self.assertEquals(2, len(application.execution_scripts))
+        self.assertEquals("cd", application.execution_scripts[0].command)
+        self.assertEquals("rm", application.execution_scripts[1].command)
+
+        # It should be only two execution scripts in the db:
+        execution_scripts = db.session.query(ExecutionScript).all()
+        self.assertEquals(2, len(execution_scripts))
+        self.assertEquals("cd", execution_scripts[0].command)
+        self.assertEquals("rm", execution_scripts[1].command)
+
