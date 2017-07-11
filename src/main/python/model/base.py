62108671c727ecbb16e15c15c28c481f0f1b4a2e
David Garcia Perez <davidgp@davidgp.com>
2017-07-11 09:33:58 +0200

    deleting unnecesary file after integrating everything into the models file

--- src/main/python/model/base.py
+++
@@ -1,8 +0,0 @@
-# Builder script for the Application Lifecycle Deployment Engine
-#
-# This is being developed for the TANGO Project: http://tango-project.eu
-#
-# Copyright: David García Pérez, Atos Research and Innovation, 2016.
-#
-# This code is licensed under an Apache 2.0 license. Please, refer to the LICENSE.TXT file for more information
-
