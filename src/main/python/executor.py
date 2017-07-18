4a8d5e9359e8435212a07a166c6ef7424dc2fa74
David Garcia Perez <davidgp@davidgp.com>
2017-07-18 14:04:39 +0200

    adding thread implementation of the execute application, so REST API can continue working

--- src/main/python/executor.py
+++ src/main/python/executor.py
@@ -6,9 +6,12 @@
 #
 # This code is licensed under an Apache 2.0 license. Please, refer to the LICENSE.TXT file for more information
 
+from threading import Thread
 from models import db, Execution
 
+execute_type_slurm_sbatch = "slurm:sbatch"
 execute_status_submitted = "SUBMITTED"
+execute_status_failed = "FAILED"
 
 def execute_application(execution_script):
 	"""
@@ -25,3 +28,27 @@ def execute_application(execution_script):
 	execution_script.executions.append(execution)
 
 	db.session.commit()
+
+	# We verify that we recoginze the type of execution
+	if execution.execution_type == execute_type_slurm_sbatch :
+		
+		t = Thread(target=execute_application_type_slurm_sbatch, args=(execution,))
+		t.start()
+		return t
+	else: 
+		execution.status = execute_status_failed
+		execution.output = "No support for execurtion type: " + execution.execution_type
+		db.session.commit()
+
+def _execute_application_type_slurm_sbatch(execution):
+	"""
+	It just starts this process in a Thread
+	"""
+
+def execute_application_type_slurm_sbatch(execution):
+	"""
+	Executes an application with a device supervisor configured
+	for slurm sbatch
+	"""
+
+	pass
--- src/unittest/python/executor_tests.py
+++ src/unittest/python/executor_tests.py
@@ -7,6 +7,7 @@
 # This code is licensed under an Apache 2.0 license. Please, refer to the LICENSE.TXT file for more information
 
 import executor
+import unittest.mock as mock
 from sqlalchemy_mapping_tests.mapping_tests import MappingTest
 from models import db, Execution, ExecutionScript
 
@@ -15,7 +16,8 @@ class ExecutorTests(MappingTest):
 	Unit test for the package Executor in charge of executing an application
 	"""
 
-	def test_execute_application(self):
+	@mock.patch("executor.execute_application_type_slurm_sbatch")
+	def test_execute_application(self, mock_slurm_sbatch):
 		"""
 		Verifies that the right methods and status are set when an appplication is executed
 		"""
@@ -24,7 +26,7 @@ class ExecutorTests(MappingTest):
 		db.session.add(execution_script)
 		db.session.commit()
 
-		executor.execute_application(execution_script)
+		t = executor.execute_application(execution_script)
 
 		execution_script = db.session.query(ExecutionScript).filter_by(command="ls").first()
 
@@ -34,3 +36,23 @@ class ExecutorTests(MappingTest):
 		self.assertEquals("slurm:sbatch", execution.execution_type)
 		self.assertEquals("-x1", execution.parameters)
 		self.assertEquals(executor.execute_status_submitted, execution.status)
+
+		# We verify that the right method was called
+		t.join()
+		mock_slurm_sbatch.assert_called_with(execution)
+
+		# We verify the wrong status of unrecognize execution type
+		execution_script.execution_type = "xxx"
+		db.session.commit()
+
+		executor.execute_application(execution_script)
+
+		execution_script = db.session.query(ExecutionScript).filter_by(command="ls").first()
+
+		self.assertEquals(2, len(execution_script.executions))
+		execution = execution_script.executions[1]
+		self.assertEquals("ls", execution.command)
+		self.assertEquals("xxx", execution.execution_type)
+		self.assertEquals("-x1", execution.parameters)
+		self.assertEquals(executor.execute_status_failed, execution.status)
+		self.assertEquals("No support for execurtion type: xxx", execution.output)
