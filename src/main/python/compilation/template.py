746c9aa012170a58e4e7c07ea1dfbbfed53c084a
David García Pérez <david.garciaperez@atos.net>
2017-08-30 14:57:04 +0200

    new unittest for checking if the singularity templates are correctly formatted

---
+++ src/main/python/compilation/template.py
@@ -0,0 +1,17 @@
+# This contains the methods that edit a templeta file
+#
+# This is being developed for the TANGO Project: http://tango-project.eu
+#
+# Copyright: David García Pérez, Atos Research and Innovation, 2016.
+#
+# This code is licensed under an Apache 2.0 license. Please, refer to the LICENSE.TXT file for more information
+
+def update_template(template, build_command, folder_location='/home/tango', app_folder='application'):
+	"""
+	It updates a template changing the following variables:
+	- build_command
+	- folder_location
+	- app_folder
+	"""
+
+	pass
--- src/unittest/python/compilation_tests/template_tests.py
+++ src/unittest/python/compilation_tests/template_tests.py
@@ -8,6 +8,7 @@
 
 import unittest
 import os
+import compilation.template as template
 
 class TemplateTests(unittest.TestCase):
 	"""
@@ -26,7 +27,30 @@ class TemplateTests(unittest.TestCase):
 
 		# We prepare the file that we are going to use for the tests
 		with open("test_update_template_variables.txt", "w") as text_file:
-			print(f"Purchase Amount", file=text_file)
+			print("Test test test file...", file=text_file)
+			print("some text{#FOLDER_LOCATION#}...", file=text_file)
+			print("Test test test file...", file=text_file)
+			print("Test {#APP_FOLDER#}test {#FOLDER_LOCATION#}...", file=text_file)
+			print("Test test test file...", file=text_file)
+			print("{#APP_FOLDER#} test {#BUILD_COMMAND#} file...", file=text_file)
+			print("end of file", file=text_file)
+
+
+		destination_template = template.update_template("test_update_template_variables.txt",
+														"gcc -x1",
+														"/home/pepito",
+														"application")
+
+		# We read all the lines
+		lines = [line.rstrip('\n') for line in open(destination_template)]
+
+		self.assertEquals("Test test test file...", line[0])
+		self.assertEquals("some text/home/pepito...", line[1])
+		self.assertEquals("Test test test file...", line[2])
+		self.assertEquals("Test applicationtest /home/pepito...", line[3])
+		self.assertEquals("Test test test file...", line[4])
+		self.assertEquals("application test gcc -x1 file...", line[5])
+		self.assertEquals("end of file", line[6])
 
 		# We clean after the test
 		os.remove("test_update_template_variables.txt")
