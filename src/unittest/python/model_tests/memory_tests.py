0fd76248a14940729f35dbd6194ef8b3722bcd12
U-WW930\a510804 <a510804@MC0XJ6TC.ww930.my-it-solutions.net>
2018-10-04 11:33:49 +0200

    updating license headers

--- src/unittest/python/model_tests/memory_tests.py
+++ src/unittest/python/model_tests/memory_tests.py
@@ -1,10 +1,22 @@
-# Builder script for the Application Lifecycle Deployment Engine
 #
+# Copyright 2018 Atos Research and Innovation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+# 
 # This is being developed for the TANGO Project: http://tango-project.eu
 #
-# Copyright: David García Pérez, Atos Research and Innovation, 2016.
+# Unit tests that checks ALDE Datamodel
 #
-# This code is licensed under an Apache 2.0 license. Please, refer to the LICENSE.TXT file for more information
 
 
 import unittest
