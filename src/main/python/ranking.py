befb4ad30fd480dd68adc9d6b9a33d80599eff2a
U-WW930\a510804 <a510804@MC0XJ6TC.ww930.my-it-solutions.net>
2018-11-22 11:57:10 +0100

    fixing license issues

--- src/main/python/ranking.py
+++ src/main/python/ranking.py
@@ -1,14 +1,21 @@
-#!/usr/bin/env python
 #
+# Copyright 2018 Atos Research and Innovation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+# 
 # This is being developed for the TANGO Project: http://tango-project.eu
 #
-# Copyright: David García Pérez, Atos Research and Innovation, 2016.
-#
-# This code is licensed under an Apache 2.0 license. Please, refer to the LICENSE.TXT file for more information
-#
-# It stores all the logic to read and create the ranking files
-# for a given execution. Using the commands provided by the
-# TANGO Self-Adaptation Manager
+# Module that stores all the ranking of different configurations
 #
 
 import csv
